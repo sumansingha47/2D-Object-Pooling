980b9de5625e61d2e491204728c4624fc840fe00
SUMAN SINGHA <sumansingha047@gmail.com>
2024-06-07 00:16:36 +0530

    Add a simple enemy and spawn it randomly

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject enemy;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Random.Range(0, 100) < 5)
+        {
+            Instantiate(enemy, this.transform.position + new Vector3(Random.Range(-10, 10), 0, 0), Quaternion.identity);
+        }
+    }
+}
